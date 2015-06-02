0e2e294cb69374d2245012b9007f7be907b3c85d
Poonkodi <poongodee@gmail.com>
2015-06-01 21:47:05 -0700

    commit

---
+++ LevelOrderTraverse.java
@@ -0,0 +1,35 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+ * Level order traversal of a binary tree
+ */
+public class LevelOrderTraverse {
+	public static void levelOrder(Node root)
+	{
+		Deque<Node> q=new ArrayDeque<Node>();
+		if(root!=null)
+		{
+		q.add(root);
+		}
+		while(!q.isEmpty())
+		{
+			Node current=q.remove();
+			System.out.println(current.data);
+			if(current.left!=null)
+			q.add(current.left);
+			if(current.right!=null)
+			q.add(current.right);
+		}				
+	}
+	
+	public static void main(String args[])
+	{
+		Node B=new Node(12,
+				new Node(13,new Node(14, new Node(114), new Node(342)),new Node(16)),
+				new Node(20, new Node(111), new Node(1234)));
+		Node C=new Node(20, new Node(111), new Node(1234));		
+		levelOrder(B);
+		levelOrder(C);
+	}
+}
--- Node.java
+++ Node.java
@@ -1,4 +1,18 @@
 
 public class Node {
+	int data;
+	Node left;
+	Node right;
 
+	Node(int data,Node left,Node right)
+	{
+		this.data=data;
+		this.left=left;
+		this.right=right;
+	}
+	
+	Node(int data)
+	{
+		this.data=data;
+	}
 }

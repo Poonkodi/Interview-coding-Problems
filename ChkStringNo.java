326b742a10f4ee2da55a95f4a2f2508aca341afd
Poonkodi <poongodee@gmail.com>
2015-06-02 23:40:08 -0700

    commit

---
+++ ChkStringNo.java
@@ -0,0 +1,28 @@
+
+public class ChkStringNo {
+	/*
+	 * Check whether a given string is a no 
+	 */
+
+	public static boolean chkIfNumber(String s)
+	{
+		char arr1[]=s.toCharArray();
+		for(int i=0;i<arr1.length;i++)
+		{
+			int no=arr1[i];
+			//System.out.println(no);
+			if(!(no>=48&&no<=57))
+				return false;
+		}
+		return true;
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.println(chkIfNumber("12344s"));
+		System.out.println(chkIfNumber("12344"));
+		System.out.println(chkIfNumber("a12344"));
+
+
+	}
+}
